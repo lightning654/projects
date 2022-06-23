4ce558a4364de3a8a98c2ee72078086ec5793a00
lightning654 <106192216+lightning654@users.noreply.github.com>
2022-06-23 22:21:19 +0100

    Add files via upload

---
+++ ASCII array/ASCII array/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASCII_array
+{
+    class Program
+    {
+        static char[,] Grid = new char[5, 5];
+
+        static void NewGrid()
+        {
+            char Letter;
+            int ASCII;
+            Random Pick = new Random();
+            for (int Y = 0; Y < 5; Y++)
+            {
+                for (int X = 0; X < 5; X++)
+                {
+                    ASCII = Pick.Next(1, 26) + 64;
+                    Letter = (char)ASCII;
+                    Grid[X, Y] = Letter;
+                }
+                Console.WriteLine(Grid);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            NewGrid();
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ adds,removes,outputs items in list/adds,removes,outputs items in list/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adds_removes_outputs_items_in_list
+{
+    class Program
+    {
+        static List<string> Pocket = new List<string>();
+
+        // Subroutine to put data into the list
+        static void PutInPocket(string Item)
+        {
+            Pocket.Add(Item);
+        }
+
+        // Subroutine to remove data from the list
+        static void TakeOutOfPocket(string Item)
+        {
+            Pocket.Remove(Item);
+        }
+        // Subroutine to output the list
+        static void ShowPocket()
+        {
+            foreach (string Item in Pocket)
+            {
+                Console.WriteLine(Item);
+            }
+        }
+
+        //Main program
+        static void Main(string[] args)
+        {
+            PutInPocket("Wallet");
+            PutInPocket("Keys");
+            PutInPocket("Tissue");
+            TakeOutOfPocket("Keys");
+            ShowPocket();
+            Console.ReadLine();
+        }
+    }
+}
+
