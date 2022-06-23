d98d870b10718ccbb4ea45d6dfb468c5f6d55718
lightning654 <106192216+lightning654@users.noreply.github.com>
2022-06-23 22:22:37 +0100

    Add files via upload

---
+++ assigns hello world to two elements/assigns hello world to two elements/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assigns_hello_world_to_two_elements
+{
+    class Program
+    {
+        // Function to output the contents of an array
+        static void OutputArray(string[] Sentence)
+        {
+            foreach (string Word in Sentence)
+            {
+                Console.Write(Word + " ");
+            }
+            Console.WriteLine();
+        }
+
+        //Main program
+        static void Main(string[] args)
+        {
+            string[] Sentence = new string[2];
+            Sentence[0] = "Hello";
+            Sentence[1] = "World";
+            OutputArray(Sentence);
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ calculates lowest common multiple/calculates least common denominator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculates_least_common_denominator
+{
+    class Program
+    {
+        // Subroutine to use trial and error to calculate LCD
+        static int LCM(int Number1, int Number2)
+        {
+            int Counter = 1;
+            while ((Counter % Number1 != 0) || (Counter % Number2 != 0))
+            {
+                Counter++;
+            }
+            return Counter;
+        }
+
+        //Main program
+        static void Main(string[] args)
+        {
+            int Number1 = 20;
+            int Number2 = 7;
+            int Result = LCM(Number1, Number2);
+            Console.WriteLine("The LCM of {0} and {1} is {2}", Number1, Number2, Result);
+            Console.ReadLine();
+        }
+    }
+}
+
