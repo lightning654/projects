7881f8e4fcee36933b6aef2716375a2ca715ec75
lightning654 <106192216+lightning654@users.noreply.github.com>
2022-06-24 20:47:59 +0100

    Add files via upload

---
+++ outputs string 10 times (2)/outputs string 10 times (2)/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace outputs_string_10_times__2_
+{
+	class Program
+    {
+        // Subroutine to demonstrate loops
+        static void DemoLoop()
+        {
+            for (int Counter = 1; Counter < 11; Counter++)
+            {
+                Console.WriteLine("This is line {0}", Counter);
+            }
+        }
+
+        //Main program
+        static void Main(string[] args)
+        {
+            DemoLoop();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ outputs string 10 times/outputs string 10 times/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace outputs_string_10_times
+{
+    class Program
+    {
+        // Subroutine to demonstrate loops
+        static void OutputMessage(string Message)
+        {
+            for (int Counter = 0; Counter < 10; Counter++)
+            {
+                Console.WriteLine(Message);
+            }
+        }
+
+        //Main program
+        static void Main(string[] args)
+        {
+            string Message = "This is how you get code to repeat.";
+            OutputMessage(Message);
+            Console.ReadLine();
+        }
+    }
+}
+
