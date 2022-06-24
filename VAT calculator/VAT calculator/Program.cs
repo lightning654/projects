b78740ba57028f3f7bb4325ed0e722ff5550186d
lightning654 <106192216+lightning654@users.noreply.github.com>
2022-06-24 20:54:47 +0100

    Add files via upload

---
+++ VAT calculator/VAT calculator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VAT_calculator
+{
+    class Program
+    {
+        // Subroutine to calculate VAT
+        static double VAT(double Total)
+        {
+            return Total * 0.05;
+        }
+
+        //Main program
+        static void Main(string[] args)
+        {
+            const double Total = 100.12;
+            double ValueAddedTax = VAT(Total);
+            double ToPay = Total + ValueAddedTax;
+            Console.WriteLine("Total £{0:0.00} :: VAT £{1:0.00} :: To Pay £{2:0.00}", Total, ValueAddedTax, ToPay);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ validates the input/validates the input/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace validates_the_input
+{
+    class Program
+    {
+        // Function to return a valid  input
+        static string GetInput()
+        {
+            Console.WriteLine("a. Option A");
+            Console.WriteLine("b. Option B");
+            bool Valid = false;
+            string Choice = "";
+            while (!Valid)
+            {
+                Console.WriteLine("Enter an option: ");
+                Choice = Console.ReadLine();
+                if ((Choice == "1") || (Choice == "2") || (Choice == "A") || (Choice == "B"))
+                {
+                    Valid = true;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid option chosen. Try again.");
+                }
+            }
+            return Choice;
+        }
+        //Main program
+        static void Main(string[] args)
+        {
+            string Choice = GetInput();
+            Console.WriteLine("You chose option {0}.", Choice);
+            Console.ReadLine();
+        }
+    }
+}
+
